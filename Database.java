1dc885119c9f1f9e407510f0aaa80b9fd327eb0e
Marc Frankel <mfrankel218@gmail.com>
2018-04-10 19:57:14 -0400

    Finished Messages checkstyle and misc updates on database file

--- Database.java
+++ Database.java
@@ -3,6 +3,7 @@ import java.time.LocalDateTime;
 import java.util.function.Predicate;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
 
 
 /**
@@ -11,3 +12,40 @@ import java.util.Map;
  * @version 1.0
  * 4/10/18
 **/
+
+public class Database {
+
+    private List<Message> messages;
+
+
+    public Database(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public List<Message> filter(Predicate<Message> filter) {
+        List<Message> outputList = new ArrayList<Message>();
+        for (Message message : messages) {
+            if (filter.test(message)) {
+                outputList.add(message);
+            }
+        }
+        return outputList;
+    }
+
+    public List<Message> getMessagesWithKeyword(String keyword) {
+        List<Message> output = messages;
+        keyword = keyword.toLowerCase();
+
+
+        return output;
+
+    }
+
+    public Message getMessage(int n) {
+        return messages.get(n);
+    }
+}
--- Message.java
+++ Message.java
@@ -17,8 +17,17 @@ public class Message {
     private LocalDateTime date;
     private boolean isImportant;
 
-
-    public Message (Optional<String> contactName, String to, String from,
+    /**
+     * Public Message contructor
+     * @param  contactName Optional contact name on the message
+     * @param  to          Who the message is to
+     * @param  from        Who the message is from
+     * @param  body        Body of the message
+     * @param  date        Date of the message
+     * @param  isImportant if the message is important
+     *
+     */
+    public Message(Optional<String> contactName, String to, String from,
         String body, LocalDateTime date, boolean isImportant) {
 
         this.contactName = contactName;
@@ -29,26 +38,50 @@ public class Message {
         this.isImportant = isImportant;
     }
 
+    /**
+     * Getter for contact name
+     * @return Returns contact name
+     */
     public Optional<String> getContactName() {
         return contactName;
     }
 
+    /**
+     * Getter for to field
+     * @return Returns to
+     */
     public String getTo() {
         return to;
     }
 
+    /**
+     * Getter for From field
+     * @return Returns from
+     */
     public String getFrom() {
         return from;
     }
 
+    /**
+     * Getter for body
+     * @return Returns body of message
+     */
     public String getBody() {
         return body;
     }
 
+    /**
+     * Getter for date of message
+     * @return Returns date of message
+     */
     public LocalDateTime getDate() {
         return date;
     }
 
+    /**
+     * Returns isImportant
+     * @return isImportant variable
+     */
     public boolean getIsImportant() {
         return isImportant;
     }
